c825f3d4e0530b0ef7241b8e9f7edbf1b27f85b6
Apaar Madan <apaar_madan@hotmail.com>
2021-04-24 13:15:45 -0400

    Bop to the beat

--- game.js
+++ game.js
@@ -104,8 +104,13 @@ function drawStars() {
 }
 
 function drawCursor() {
+    const bpm = 120;
+    const bps = bpm / 60;
+
+    const scaleFactor = Math.sin(Math.PI * 2 * bps * timeElapsedSeconds) * 0.1 + 1;
+
     const cursorCoords = [0, 0, 15, 50, 30, 0, 15, 20].map((elem, ind) =>
-        ind % 2 ? elem + CURSOR_Y : elem + mouseX - 15
+        ind % 2 ? (elem * scaleFactor) + CURSOR_Y : (elem - 15) * scaleFactor + mouseX
     );
 
     fillLine(cursorCoords, CURSOR_COL, 2, true);
