5d564a9b6c6fca66e878aaa3e7b93846247204cd
Apaar Madan <apaar_madan@hotmail.com>
2021-04-24 00:40:38 -0400

    Add fillRect function

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+let canvas = null;
+let ctx = null;
 
 function setupCanvas() {
-    const canvas = document.getElementById('canvas');
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
     function resizeCanvas() {
         canvas.height = window.innerHeight;
@@ -11,13 +14,14 @@ function setupCanvas() {
     resizeCanvas();
 }
 
-function render() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+function fillRect(x, y, w, h, col = "#fff") {
+    ctx.fillStyle = col;
+    ctx.fillRect(x, y, w, h);
+}
 
+function render() {
     // Fill background
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    fillRect(0, 0, canvas.width, canvas.height, "#000"); 
 
     window.requestAnimationFrame(render);
 }
