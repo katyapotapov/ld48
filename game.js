f3b524ecfe82ab04edabd02a59168ff218a27884
katyapotapov <kat.p28@gmail.com>
2021-04-24 00:51:22 -0400

    Add event listener for mouse coords

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 let canvas = null;
 let ctx = null;
 
+let mouseX = 0;
+let mouseY = 0;
+
 function setupCanvas() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -19,15 +22,25 @@ function fillRect(x, y, w, h, col = "#fff") {
     ctx.fillRect(x, y, w, h);
 }
 
+function attachMouseListener() {
+    canvas.addEventListener("mousemove", (event) => {
+        const rect = canvas.getBoundingClientRect();
+        mouseX = event.clientX - rect.left;
+        mouseY = event.clientY - rect.top;
+    });
+}
+
 function render() {
     // Fill background
     fillRect(0, 0, canvas.width, canvas.height, "#000"); 
+    console.log(mouseX, mouseY);
 
     window.requestAnimationFrame(render);
 }
 
 function init() {
     setupCanvas();
+    attachMouseListener();
     render();
 }
 
